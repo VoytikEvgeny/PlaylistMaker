40ccf988e5497f3a393fcfa29d9e8eb523d497ed
evoytik <evoytik@gmail.com>
2026-04-01 23:11:28 +0300

    Спринт №11
    Signed-off-by: evoytik <evoytik@gmail.com>

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -9,9 +9,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-
-class TrackViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val ivArtwork: ImageView = itemView.findViewById(R.id.ivArtwork)
     private val tvTrackName: TextView = itemView.findViewById(R.id.tvTrackName)
@@ -21,21 +22,25 @@ class TrackViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
     fun bind(item: Track) {
 
         tvTrackName.text = item.trackName
+        tvArtistName.setText("")
         tvArtistName.text = item.artistName
-        tvTrackTime.text = item.trackTime
+        tvTrackTime.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(item.trackTimeMillis)
 
         Glide.with(itemView.context)
             .load(item.artworkUrl100)
             .placeholder(R.drawable.ic_placeholder_45)
             .error(R.drawable.ic_placeholder_45)
-            .apply(RequestOptions.bitmapTransform(RoundedCorners(dpToPx(8.0f,itemView.context))))
+            .apply(RequestOptions.bitmapTransform(RoundedCorners(dpToPx(8.0f, itemView.context))))
             .fitCenter()
             .into(ivArtwork)
 
     }
+
     private fun dpToPx(dp: Float, context: Context): Int {
         return TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP,
             dp,
-            context.resources.displayMetrics).toInt()
-    }}
+            context.resources.displayMetrics
+        ).toInt()
+    }
+}
